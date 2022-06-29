c619746242d9c3ee9e346fd1721969cc872b9a67
soyeon207 <psy010207@naver.com>
2022-06-30 03:03:53 +0900

    Feat: 프로젝트 init (#1)

---
+++ src/main/java/com/backpacker/memberorder/MemberOrderApplication.java
@@ -0,0 +1,13 @@
+package com.backpacker.memberorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backpacker/memberorder/MemberOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backpacker.memberorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
